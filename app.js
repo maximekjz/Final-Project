0969d485a560f1f5461ad616211c887d6830995a
sarahkossmann <59928941+sarahkossmann@users.noreply.github.com>
2024-08-27 13:34:22 +0200

    Auth in process

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const userRouter = require("./routes/userRouter.js");
+const cookieParser = require("cookie-parser")
+const cors = require('cors')
+const app = express();
+
+require('dotenv').config();
+
+// body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5000',
+  credentials: true
+}));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`run on ${PORT}`);
+});
+
+app.use("/user", userRouter);
---
+++ client/src/components/LoginRegister.jsx
@@ -0,0 +1,9 @@
+const LoginRegister = ({title}) => {
+    return(
+        <>
+            <h2>{title}</h2>
+        </>
+    )
+}
+
+export default LoginRegister
